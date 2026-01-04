11268ea6105d4108ad4416bcad1d409d2ab4e496
bhurtelamrit211-rgb <bhurtelamrit211@gmail.com>
2026-01-04 22:38:02 +0545

    Update MessageScreen.jsx

--- src/components/screens/MessageScreen.jsx
+++ src/components/screens/MessageScreen.jsx
@@ -27,14 +27,13 @@ export default function MessageScreen({ onNext }) {
                     className=" h-auto max-w-xl bg-gradient-to-br from-pink-200 via-pink-100 to-pink-50 rounded-2xl shadow-lg p-4 md:p-6 text-center"
                 >
                     <p className="text-[#301733] text-base md:text-lg leading-relaxed overflow-y-auto max-h-[400px] pr-2">
-                        Happy Birthday, ABC 🎂💖✨
+                        Happy Birthday, Aayusha 🎂💖✨
                         You make the world brighter just by being you 🌸 your smile 😊, your kindness 💫, and the warmth you bring to every heart 💞.
-                        Thank you Meri Dunia mein aany kalie.....
                         I hope today wraps you in love 🤍, laughter 😄, and beautiful surprises 🎁✨.
                         You’re truly one of a kind 💎, and my heart smiles thinking of you 💓.
                         Stay amazing 🌟, stay magical ✨… and never forget how special you are 🥰💫
 
-                        Guroooor 😄💓
+                        Take care 😄💓
                     </p>
                 </motion.div>
             </div>
